e83a3219c9a3db24838f216ee0e82ca04d263c42
Gkreischer <gustavokreischer@gmail.com>
2021-09-28 17:50:27 -0300

    Criado area de login e organizado rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/centralCliente/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '**', component: PaginaNaoEncontradaComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { ServicosComponent } from './components/servicos/servicos.component';
 import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './components/centralCliente/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { ContatoComponent } from './components/contato/contato.component';
     CarouselComponent,
     SobreComponent,
     ServicosComponent,
-    ContatoComponent
+    ContatoComponent,
+    LoginComponent,
+    HomeComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
