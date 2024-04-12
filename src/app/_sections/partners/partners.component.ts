afdf02004bf1f216a8d8ac7ff8c2f99a5fe9c89a
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2024-04-11 23:06:18 -0300

    change: partners section, add: logo and some images

--- src/app/_sections/partners/partners.component.ts
+++ src/app/_sections/partners/partners.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.scss']
 })
-export class PartnersComponent {
+export class PartnersComponent implements OnInit {
+
+
+  swiperParams: any = {
+    slidesPerView: 1,
+    breakpoints: {
+      1366: {
+        slidesPerView: 4,
+      }
+    }
+  };
+
+  ngOnInit(): void {
+    const swiperEl = document.getElementById('swiperContainerPartners');
+
+    if(swiperEl) {
+      Object.assign(swiperEl, this.swiperParams)
+    }
+  }
 
 }
